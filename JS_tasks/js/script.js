7f92051b919d137c6b2b377c790ca62da91616e6
stasokoleso <magic.dast@gmail.com>
2020-09-18 20:33:11 +0300

    add next tasks

--- JS_tasks/js/script.js
+++ JS_tasks/js/script.js
@@ -1,4 +1,23 @@
-// 4.2.1 Модуль числа
+// 4.2.1, 4.4.1 Модуль числа (сразу писал тернарный)
 let value = parseInt(prompt('Please, enter any number'));
 
 value >= 0 ? console.log(value) : console.log(-value);
+
+// 4.3.1 Камень, ножницы, бумага
+const rock = 0;
+const scissors = 1;
+const paper = 2;
+
+// попросим игрока ввести ответ в виде числа
+let player = parseInt(prompt('Your choice: 0 - rock, 1 - scissors, 2 - paper'));
+// Рандомное число 0, 1 или 2 — разберем это позже, пока не заморачивайтесь
+let computer = Math.floor(Math.random() * 3);
+
+// опишем все условия и будем выводить в консоль 'computer win' или 'player win'
+if (player == computer) {
+	console.log(`Draw! Try again.`)
+} else if (((player == rock) && (computer == scissors)) || ((player == scissors) && (computer == paper)) || ((player == paper) && (computer == rock))) {
+	console.log(`Congratulations, you win!`);
+} else {
+	console.log(`Looser! Try again.`)
+};
