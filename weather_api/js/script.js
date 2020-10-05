430c821e8e3c5ffff0149aeb690fd98164eb8ed2
stasokoleso <magic.dast@gmail.com>
2020-10-05 09:49:32 +0300

    Update script.js

--- weather_api/js/script.js
+++ weather_api/js/script.js
@@ -6,13 +6,12 @@ let xhr = new XMLHttpRequest();
 submit.onclick = function (e) {
 	e.preventDefault();
 
-	var city = document.querySelector('input').value;
+	let city = document.querySelector('input').value;
 	const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
 	xhr.open('GET', url, false);
 	xhr.send();
 	let DATA = JSON.parse(xhr.responseText);
-	// console.log(DATA); Проверочный)
 	document.querySelector('.text').innerHTML = `Temperature in ${city} is ${Math.round(
 		DATA.main.temp - 273
 	)}℃ and Wind speed is ${DATA.wind.speed} m/s`;
